698aa9b8b3478c4209b75e0a6a197466042e829c
Giacomo-Madrid <gsmr.18@gmail.com>
2023-11-14 20:17:18 -0500

    Postfix >1 cifra

--- funciones.js
+++ funciones.js
@@ -120,37 +120,45 @@ function infixToPrefix(x) {
     return reverseString(prefix);
 }
 
-function infixToPostfix(x) {
-    const infix = this.changeOperators(x.toUpperCase());
+function infixToPostfix(expression) {
     const stack = new Stack();
     let postfix = "";
 
-    for (let i = 0; i < infix.length; i++) {
-        const char = infix.charAt(i);
+    for (let i = 0; i < expression.length; i++) {
+        const char = expression.charAt(i);
 
         if (this.isLetterOrDigit(char)) {
             postfix += char;
+
+            let j = i + 1;
+            while (j < expression.length && this.isLetterOrDigit(expression.charAt(j))) {
+                postfix += expression.charAt(j);
+                j++;
+            }
+            i = j - 1;
+            postfix += " ";
         } else if (char === '(') {
             stack.insert(char);
         } else if (char === ')') {
             while (!stack.isEmpty() && stack.peek() !== '(') {
-                postfix += stack.pop();
+                postfix += stack.pop() + " ";
             }
             stack.pop();
-        } else {
+        } else if (this.isOperator(char)) {
             while (!stack.isEmpty() && this.getPrecedence(char) <= this.getPrecedence(stack.peek())) {
-                postfix += stack.pop();
+                postfix += stack.pop() + " ";
             }
             stack.insert(char);
         }
     }
 
     while (!stack.isEmpty()) {
-        postfix += stack.pop();
+        postfix += stack.pop() + " ";
     }
-    return postfix;
-    
+
+    return postfix.trim();
 }
+
 function getOperands(expression) {
     const operands = [];
     for (let i = 0; i < expression.length; i++) {
