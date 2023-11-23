092dc356b595d87701ec9109da00d4352fa4ada7
LuisLTdev <135352503+LuisLTdev@users.noreply.github.com>
2023-11-23 11:05:47 -0500

    Update principal.js

--- principal.js
+++ principal.js
@@ -11,7 +11,7 @@ function principal(event) {
 function procesar(entrada) {
     // Set de expresiones
     let comandos = ["evaluate", "solve", "simplify", "boolean table", "prefix", "postfix", "factorial",
-        "combinatorics repetition", "variation repetition", "combinatorics", "variation", "sort", "divisors"];
+        "combinatorics repetition", "variation repetition", "combinatorics", "variation", ];
 
     for (let i = 0; i < comandos.length; ++i) {
         let comando = comandos[i];
@@ -30,9 +30,6 @@ function procesar(entrada) {
             else if (comando == comandos[8]) return "Resultado: " + variationRepetition(expresion);
             else if (comando == comandos[9]) return "Resultado: " + combinatorics(expresion);
             else if (comando == comandos[10]) return "Resultado: " + variation(expresion);
-            else if (comando == comandos[11]) return "Resultado: " + heapsortSorter(expresion);
-            else if (comando == comandos[12]) return "Resultado: " + divisorsFunction(expresion);
-
 
             else return "¡Error! No se puede procesar la expresión!"
         }
